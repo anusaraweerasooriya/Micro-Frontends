ef2274177cdf7fe059d1ec850f034e0315a0d960
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-13 19:47:27 +0530

    Small change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello Hi there there!</h1>
+      <h1>Hello Hi with by me there there!</h1>
       <hr />
       <MarketingApp />
     </div>
