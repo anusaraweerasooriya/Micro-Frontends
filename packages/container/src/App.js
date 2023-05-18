3e91884138d7e239eff183a05f48c43a1f7578b4
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-18 16:12:04 +0530

    Build 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello there is me here!</h1>
+      <h1>Hello there is is me here!</h1>
       <hr />
       <MarketingApp />
     </div>
